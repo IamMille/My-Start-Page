59357140cdce60775885d06c09c1ec27744cee2b
feldtsen <feldtsen@gmail.com>
2017-05-08 07:47:24 +0200

    stash

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
             uid: false,
             authPopup: false,
             username: '',
-            darkTheme: false
+            darkTheme: true
         }
     }
     //handle auth
